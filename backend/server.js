88735bd958fbc0826fb24cebee421ebcab830466
Rahul Ch <xavi3441@gmail.com>
2024-12-20 21:12:51 +0530

    Set DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import http from "http";
+import connectDB from "./db/db.js";
 
 
 const PORT=process.env.PORT || 3000;
 const server=http.createServer(app);
 
 server.listen(PORT,()=>{
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
